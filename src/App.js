100125ff3a39689f5517fb5d897d08086b9cd233
Raz <razsh98@gmail>
2020-01-02 15:17:28 +0200

    Navigation completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
 import Navigation from "./components/layout/Navigation";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
         <div className="App">
-          <Navigation />
+            <Navigation />
         </div>
-      </BrowserRouter>
     );
   }
 }
--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,32 +1,31 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-// import { Link, Route, Switch} from 'react-router-dom';
-// import Home from "./Home"
-// import Tutorial from "./Tutorial";
-// import ManageAcc from "./ManageAcc";
-// import SignOut from "./SignOut";
+import './colors.css';
 
 class Navigation extends Component{
     render() {
         return (
-            <Navbar bg={"dark"} variant={"dark"}>
-                <div className={"row"}>
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/">Tutorial</Nav.Link>
-                </div>
-                <div className={"row justify-content-center"}>
-                    <Navbar.Brand>
-                        <Nav.Link href="/">
-                            Hugobot
-                        </Nav.Link>
-                    </Navbar.Brand>
-                </div>
-                <div className={"row"}>
-                    <Nav.Link href="/">Account</Nav.Link>
-                    <Nav.Link href="/">Sign Out</Nav.Link>
-                </div>
-            </Navbar>
+            <Router>
+                <Navbar className={"bg-hugobot"} variant={"dark"}>
+                    <div className={"navbar navbar-left"}>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/">Tutorial</Nav.Link>
+                    </div>
+                    <div className={"row justify-content-center"}>
+                        <Navbar.Brand>
+                            <Nav.Link href="/">
+                                <h4>HugoBot</h4>
+                            </Nav.Link>
+                        </Navbar.Brand>
+                    </div>
+                    <div className={"navbar navbar-right"}>
+                        <Nav.Link href="/">Account</Nav.Link>
+                        <Nav.Link href="/">Sign Out</Nav.Link>
+                    </div>
+                </Navbar>
+            </Router>
         );
     }
 }
