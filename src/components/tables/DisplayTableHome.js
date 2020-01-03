02615054ec907688c5b50c3bc8a269ef31e98905
Raz <razsh98@gmail>
2020-01-03 18:09:19 +0200

    json filled with examples

--- src/components/tables/DisplayTableHome.js
+++ src/components/tables/DisplayTableHome.js
@@ -1,32 +1,32 @@
 import React, { Component } from "react";
+import Table from "react-bootstrap/Table";
 
-export class DisplayTableHome extends Component {
+class DisplayTableHome extends Component {
 
-renderTableData=()=> {
-  return this.props.HomeTable.rows.map((iter) => {
-     return (
-        <tr >
-           <td>{iter.userId}</td>
-           <td>{iter.firstName}</td>
-           <td>{iter.lastName}</td>
-           <td>{iter.phoneNumber}</td>
-           <td>{iter.emailAddress}</td>
-        </tr>
-     )
-  })
-}
-  render() {
-    return (
-      <div>
-          <h1 id='title'>React Dynamic Table</h1>
-          <table id='students'>
-              <tbody>
+    renderTableData=()=> {
+        return this.props.HomeTable.rows.map((iter) => {
+            return (
+                <tr>
+                    <td>{iter.UserID}</td>
+                    <td>{iter.DatasetName}</td>
+                    <td>{iter.Category}</td>
+                    <td>{iter.Size}</td>
+                    <td>{iter.Owner}</td>
+                    <td>{iter.PublicPrivate}</td>
+                </tr>
+            )
+        })
+    };
+
+    render() {
+        return (
+            <Table striped={true} bordered={true} hover={true} size={"md"}>
+                <tbody>
                 {this.renderTableData()}
-              </tbody>
-          </table>
-      </div>
-    )
-  }
+                </tbody>
+            </Table>
+        )
+    }
 }
 
 export default DisplayTableHome ;
