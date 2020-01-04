2dbb8314a2762297ed09c3d64582f6cbad4d5922
Raz <razsh98@gmail>
2020-01-04 15:12:53 +0200

    tutorial continued

--- src/components/content/Tutorial.js
+++ src/components/content/Tutorial.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
 
 function importAll(r) {
     return r.keys().map(r);
@@ -32,6 +36,42 @@ class Tutorial extends Component{
                 we might also have a gender class variable.
                 <br/>
                 A feasible representation of the data might be as follows:
+                <br/>
+                <br/>
+                <Image src={images[0]}/>
+                <br/>
+                <br/>
+                To transition to HugoBot’s format, we must dissect our data
+                and map each feature to its equivalent form in HugoBot’s format.
+                <br/>
+                <br/>
+                A record in HugoBot is composed of the following fields:
+                <br/>
+                <br/>
+                <ListGroup>
+                    <ListGroupItem>
+                        Entity ID - The ID of the subject, or entity on which the recordings are made. An
+                        example could be an individual patient, individual object etc.
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        Temporal Property ID -
+                        The ID of the variable that measures the state of a certain feature on the current
+                        <br/>
+                        An example could be <strong>Patient</strong> (our entity)
+                        <strong>Temperature</strong> (our feature).
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        Timestamp - A whole, non-negative number that is used to describe the current time
+                        point in which the measurements are made.
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        Temporal Property Value - The current value of the temporal property in our entity
+                        at the current timestamp. For example, our patients temperature at time 0 could be
+                        36.7 degrees celsius.
+                    </ListGroupItem>
+                </ListGroup>
+                <br/>
+                <br/>
                 <br/>
                 <br/>
             </Container>
