5737fe71721e672f5704505ed77408c4bb136a87
Raz <razsh98@gmail.com>
2020-05-21 11:21:27 +0300

    replaced upper buttons in the upload module with, shouldn't let user skip ahead of required stages now

--- src/Components/Content/UploadDataset/UploadWorkflow.js
+++ src/Components/Content/UploadDataset/UploadWorkflow.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import {ButtonGroup, Container} from "react-bootstrap";
-import {Link, Router} from "react-router-dom";
+import {Router} from "react-router-dom";
 
 import History from "../../../History";
 import '../../../resources/style/upload_workflow.css'
@@ -13,18 +13,18 @@ class UploadWorkflow extends Component{
             <Container fluid={true}>
                 <Router history={History}>
                     <ButtonGroup  size={"lg"}>
-                        <Link to={"/Upload/Metadata"} className="btn btn-upload btn-hugobot">
+                        <div className="btn btn-upload btn-hugobot">
                             Step-1 Upload Dataset File
-                        </Link>
-                        <Link to={"/Upload/VMap"} className="btn btn-upload btn-hugobot">
+                        </div>
+                        <div className="btn btn-upload btn-hugobot">
                             Step-2 Upload VMap File
-                        </Link>
-                        <Link to={"/Upload/Entities"} className="btn btn-upload btn-hugobot">
+                        </div>
+                        <div className="btn btn-upload btn-hugobot">
                             Step-3 Upload Entities File
-                        </Link>
-                        <Link to={"/Upload/WindowsCreation"} className="btn btn-upload btn-hugobot">
+                        </div>
+                        <div className="btn btn-upload btn-hugobot">
                             Step-4 Window Creation
-                        </Link>
+                        </div>
                     </ButtonGroup>
                 </Router>
             </Container>
