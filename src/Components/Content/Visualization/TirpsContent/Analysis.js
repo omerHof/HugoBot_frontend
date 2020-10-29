3fb92ef2c0fa9d99d3f1339ae28106ce0d6445c1
roikatz16 <kroi@post.bgu.ac.il>
2020-10-29 15:35:52 +0200

    #11 Navigation step 5

---
+++ src/Components/Content/Visualization/TirpsContent/Analysis.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Analysis extends Component{
+    render() {
+        return(
+            <div>Analysis</div>
+        )
+    }
+}
+
+export default Analysis;
---
+++ src/Components/Content/Visualization/TirpsContent/TIRPsSearch.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class TIRPsSearch extends Component{
+    render() {
+        return(
+            <div>TIRPsSearch</div>
+        )
+    }
+}
+
+export default TIRPsSearch;
